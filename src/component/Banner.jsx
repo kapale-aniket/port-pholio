a90093b9fcb77bb1c665b159e5fa5ca5d46c3781
kapale-aniket <aniketkaple2002@gmail.com>
2025-09-18 15:02:59 +0530

    skills updated

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -18,20 +18,32 @@ const Banner=()=>{
             <h2 className='mt-2 text-3xl font-bold'>Aniket Arun Kapale</h2>
             <h2 className='mt-2 text-xl '>I AM A FULL STACK WEB DEVLOPER</h2>
             <p className='mt-2 text-1/2xl'>
-            proficient in identifying software defect executing test cases and ensuring product quality.
-            enthusiastic about learning and applying new software development tools and methodologies .
-            </p>
+            Full Stack Developer with hands-on experience in Python, SQL, and front-end technologies. Proficient in writing clean, maintainable code, executing test cases, and ensuring product reliability. Driven by curiosity to learn new frameworks and tools while applying best practices in software development.
+            </p><br />
             <div className='icons_conainer space-x-7 text-4xl flex'>
-            <a className=' hover:bg-orange-700 border cursor-pointer px-3 py-3 w-14 h-14 rounded-full flex justify-center items-center bg-gray-800'>
-             <i class="fa-brands fa-facebook"></i>
-             </a>
-            <a className='hover:bg-orange-700 border cursor-pointer px-3 py-3w-14 h-14 rounded-full flex justify-center items-center bg-gray-800'>
-                <i class="fa-brands fa-instagram"></i>
-            </a>    
-            <a className='hover:bg-orange-700 border cursor-pointer px-3 py-3w-14 h-14 rounded-full flex justify-center items-center bg-gray-800'>
-                <i class="fa-brands fa-linkedin"></i>
-             </a>
+ 
+
+                <a
+                 href="https://www.linkedin.com/in/aniket-kapale-9b0578281/"
+                 target="_blank"
+                 rel="noopener noreferrer"
+                 className="hover:bg-blue-700 border cursor-pointer px-3 py-3 w-14 h-14 rounded-full flex justify-center items-center bg-blue-600"
+                >
+                 <i className="fa-brands fa-linkedin text-white text-2xl"></i>
+                </a>
+
+
+                <a
+                 href="https://github.com/kapale-aniket"
+                 target="_blank"
+                 rel="noopener noreferrer"
+                 className="hover:bg-orange-700 border cursor-pointer px-3 py-3 w-14 h-14 rounded-full flex justify-center items-center bg-gray-800"
+                >
+                 <i className="fa-brands fa-github text-white text-2xl"></i>
+                </a>
+
             </div >
+
             <br></br>
             <a className=' text-xl px-1 py-2 bg-orange-500 rounded-full' href="/Contact">Contact Me</a>
             </div>
